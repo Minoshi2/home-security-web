9a6a5c38f96c1de237d7515f46e8349eaf0f4c0d
Sahas <sahasmcg2995@gmail.com>
2025-03-30 13:03:19 +0530

    fix url

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 // export const BACKEND_BASE_URL = "https://home-security-api-365895512870.us-central1.run.app"
-export const BACKEND_BASE_URL = "http://127.0.0.1:5000"
-export const BACKEND_BASE_URL_WS = "http://127.0.0.1:5000"
+export const BACKEND_BASE_URL = "https://home-security-api-365895512870.us-central1.run.app"
+export const BACKEND_BASE_URL_WS = "https://home-security-api-365895512870.us-central1.run.app"
